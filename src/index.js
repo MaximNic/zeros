50d764979be8cafc0fb152c98303dcf013026e6b
MaximNic <maxlabnk@gmail.com>
2018-02-24 20:25:50 +0300

    ssd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+zeros=0;
+for (num=5; num<=number; num=num*5)
+  zeros=zeros+Math.floor(number/num)
+ return zeros;
+ }
+
